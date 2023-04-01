03270a8025f76a6bc0e552f1630c9175a9f151e5
Leonardo Cabral <leonardolccabral@gmail.com>
2023-04-01 21:20:49 +0000

    feat: create about page

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Blog - About</title>
+            </Head>
+            <main className="ml-8">
+                <h1 className="text-6xl text-gray-800">About</h1>
+            </main>
+        </>
+    )
+}
