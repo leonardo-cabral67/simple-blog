f97e16061cdeeeda53b981c8fc8ebe34b30432ea
Leonardo Cabral <leonardolccabral@gmail.com>
2023-04-01 21:21:22 +0000

    feat: list of posts

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
     return (
@@ -8,7 +9,15 @@ export default function Home() {
                 <title>Blog</title>
             </Head>
             <main className="ml-8">
-                <h1 className="text-6xl text-gray-800">Leonardo's Blog</h1>
+                <h1 className="text-6xl text-gray-800 mb-12">Leonardo's Blog</h1>
+                <h2 className="text-4xl text-gray-700 mb-6">
+                    Posts: 
+                </h2>
+                <ul className="ml-10">
+                    <li className="text-lg text-blue-400">
+                        <Link href="/posts/first-post">First post</Link>    
+                    </li>
+                </ul>
             </main>
         </>
     )
